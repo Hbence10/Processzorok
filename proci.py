a201cc587dc84fb303c3ff25e15efdc91ec61eba
Hbence10 <93594678+Hbence10@users.noreply.github.com>
2023-02-07 08:18:55 +0100

    asd

--- proci.py
+++ proci.py
@@ -1,2 +1,18 @@
 file=open("processzorok.txt", "r")
 file_data=[]
+
+2
+1
+5
+
+jatekosok = open("jatekos2.txt","r",encoding="UTF-8")
+Ljatekosok = []
+poszt = []
+
+for jatek in jatekosok:
+    if jatek[-1] == "\n":
+        Ljatekosok.append(jatek[:-1].split("\t"))
+    else:
+         Ljatekosok.append(jatek[:-1].split("\t"))
+
+del Ljatekosok[0]
