c9696c6f36ac6c325fe8c9514d4b0b17f9dffaab
Hbence10 <93594678+Hbence10@users.noreply.github.com>
2023-02-07 09:51:03 +0100

    asd

--- proci.py
+++ proci.py
@@ -1,7 +1,7 @@
 '''Tomi: 3,4,6 Hbence: 1,2,5'''
 
 
-file = open("processzorok.txt", "r")
+file = open("asd.txt", "r")
 ujproci=open("ujproci.txt", "w")
 file_data=[]
 márka = []
@@ -23,15 +23,16 @@ del file_data[0]
 
 for i in range(len(file_data)):
     márka.append(file_data[i][1])
-
-print(len(márka))
-
-#3
-
+print("Ennyi darab processzor van: ",(len(márka)))
 
 #2
 print("AMD db száma: ", márka.count("AMD"))
 print("Intel db száma: ", márka.count("Intel"))
+
+#3
+for i in range(len(file_data)):
+    if "Intel" not in file_data[i][1] and "AMD" not in file_data[i][1]:
+        print(file_data[i][1],"Ez a fajta processzor az ami nem Intel vagy AMD")
 
 #4
 for i in range(len(file_data)):
@@ -47,3 +48,4 @@ print("Százalékosan ennyi AMD található:",len(márka)/100*AMD,"%")
 #6
 for i in range(len(file_data)):
     ujproci.write(file_data[i][1] + " " + file_data[i][2] + "\n")
+   
