e1bcdaf0eedc0343c9d664adad7c17b8ab7ed5b7
TamasHornicz <tamashornicz@gmail.com>
2023-02-07 08:22:08 +0100

    asd0

--- proci.py
+++ proci.py
@@ -4,15 +4,12 @@ file_data=[]
 2
 1
 5
-
-jatekosok = open("jatekos2.txt","r",encoding="UTF-8")
-Ljatekosok = []
-poszt = []
-
-for jatek in jatekosok:
-    if jatek[-1] == "\n":
-        Ljatekosok.append(jatek[:-1].split("\t"))
+for i in file:
+    if i[-1] == "\n":
+        file_data.append(i[:-1].split("\t"))
     else:
-         Ljatekosok.append(jatek[:-1].split("\t"))
+         file_data.append(i[:-1].split("\t"))
 
-del Ljatekosok[0]
+del file_data[0]
+
+
